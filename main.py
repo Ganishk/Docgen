9d4ef8e73a23975227c8d365866a0d1b3c9fa6a2
Ganishk <ganishk1@gmail.com>
2023-02-26 15:12:49 +0530

    improved

--- main.py
+++ main.py
@@ -2,14 +2,16 @@
 
 import sys
 import sphinx
-from glob import glob #or os.walk
-
+from glob import iglob as glob #or os.walk
+#imported iglob for iterator
 
 
 
 def main():
-    project_dir = sys.argv[1] if len(sys.argv)>1 else "."
-    print(project_dir)
+    project_dir = sys.argv[1] if len(sys.argv)>1 else "." #only 1 directory is allowed at a time
+    print(f"""
+================={project_dir}=============="
+""")
     for name in glob(project_dir+"/**",recursive=True): print(name)
 
 
