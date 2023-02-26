9652cf6b70382c1153ecf0a0f53b30446b6dfca7
Ganishk <ganishk1@gmail.com>
2023-02-26 15:29:02 +0530

    to ignore unneccesary files

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 import sys
 import sphinx
+import re #regex to easily detect required format of string
+import os
 from glob import iglob as glob #or os.walk
 #imported iglob for iterator
 
@@ -9,10 +11,14 @@ from glob import iglob as glob #or os.walk
 
 def main():
     project_dir = sys.argv[1] if len(sys.argv)>1 else "." #only 1 directory is allowed at a time
-    print(f"""
-================={project_dir}=============="
-""")
-    for name in glob(project_dir+"/**",recursive=True): print(name)
+    print(f"""{project_dir}""".center(os.get_terminal_size()[0],"="))
+
+    with open(".docignore") as file: ignoring = file.read().split()
+
+    for name in glob(project_dir+"/**",recursive=True):
+        for ignore in ignoring:
+            if name.endswith(ignore): print("[!!!]",end=" ")
+        print(name)
 
 
 
