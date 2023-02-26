cd1c8aeaa1e15314486d63ab2ba8eb3babb9d037
Ganishk <ganishk1@gmail.com>
2023-02-26 15:29:34 +0530

    added an comment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-import sphinx
+import sphinx #to generate html from documentation
 import re #regex to easily detect required format of string
 import os
 from glob import iglob as glob #or os.walk
