57397ac508f52143455bae7a35ee1a04ecb820a2
Ganishk <ganishk1@gmail.com>
2023-02-26 15:00:16 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+import sphinx
+from glob import glob #or os.walk
+
+
+
+
+def main():
+    project_dir = sys.argv[1] if len(sys.argv)>1 else "."
+    print(project_dir)
+    for name in glob(project_dir,recursive=True): print(name)
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
