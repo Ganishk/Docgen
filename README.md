1aaef9fac50d6a3bd17e69f40b8f01a7211d402f
Ganishk <ganishk1@gmail.com>
2023-02-26 15:01:08 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Docgen
+### 114121031 - Ganishk D
+
+##### Code - Document Generator
+
+It is written in python
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from glob import glob #or os.walk
 def main():
     project_dir = sys.argv[1] if len(sys.argv)>1 else "."
     print(project_dir)
-    for name in glob(project_dir,recursive=True): print(name)
+    for name in glob(project_dir+"/**/",recursive=True): print(name)
 
 
 
